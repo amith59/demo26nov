c4496f76389af7b98ff651040e63e9373a6b923d
srilekha2831 <srilekhareddy2831@gmail.com>
2024-11-26 15:11:10 +0530

    dsd

---
+++ app1.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<?php
+$members = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+
+foreach ($members as $x => $y) {
+  echo "$x : $y <br>";
+}
+?>
+
+</body>
+</html>
+changes done a@ 2:30
+code for login
+again updated the data changes at 4:00
